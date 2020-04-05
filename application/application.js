a0fe64f90c003a4dc361cac1185a0c226cb58d38
Abdirahman <abrman2012@hotmail.com>
2020-04-05 16:01:51 +0200

    mods to follow archi

--- application/application.js
+++ application/application.js
@@ -27,7 +27,7 @@ class Application extends EventEmitter {
       console.log("Connected the application.");
       
       /* Subscribe to temperature event on device types of IBM-KTH-Demo and where the device ID is 0. */
-      that.app_client.subscribeToDeviceEvents("IBM-KTH", "0", "device_update_message_request");
+      that.app_client.subscribeToDeviceEvents("IBM-KTH", "0", "getCurrentMessage");
       
       /* On a data recieved, emit event. */
       that.app_client.on("deviceEvent", async function (deviceType, deviceId, eventType, format, payload) {
